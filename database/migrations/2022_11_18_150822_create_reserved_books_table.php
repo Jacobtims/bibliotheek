e34cd576e4a1a97534249e5a5fb12c712f14bdb0
Jacob <jacobtimmer04@gmail.com>
2022-11-18 15:11:31 +0100

    Add reserved books table

---
+++ database/migrations/2022_11_18_150822_create_reserved_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reserved_books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('book_id')->constrained();
+            $table->timestamps();
+
+            $table->unique(['user_id', 'book_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reserved_books');
+    }
+};
